d67db4a21df6aee0b58cce1ffa1a1df8a99db0b8
zundullahbd <uluzundullah@gmail.com>
2023-12-20 23:29:03 +0700

    dev: testing api

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import tensorflow as tf
+import uvicorn
 
 
-app = FastAPI()
+app = FastAPI(
+    description="This is the main app for the capstone project.",
+    title="Capstone App",
+    docs_url="/"
+)
 
-print("Loading model...")
+@app.post('/signup')
+async def create_account():
+    pass
+
+@app.post('/login')
+async def access_token():
+    pass
+
+@app.post('/validate')
+async def validate_token():
+    pass
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
